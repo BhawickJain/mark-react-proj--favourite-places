39edd6e9d20e69c16248c95207733f6d12da5ce7
Bhawick Jain <bhawick@gmail.com>
2022-07-18 15:36:43 +0100

    add two more favourite places

--- src/utils/placesData.ts
+++ src/utils/placesData.ts
@@ -23,6 +23,29 @@ const placesData: Place[] = [
     link: new URL("https://goo.gl/maps/4FEnMiD13J2vCDP28"),
     description: "The Library inside is wonderful and but it's all uphill!",
   }
+,
+  {
+    title: "Airbus Toulouse Factory",
+    place: "Toulouse",
+    country: "France",
+    image: {
+      source: new URL("https://www.airlive.net/wp-content/uploads/2020/01/AIRBUS-MOBILE-FINAL-ASSEMBLY-LINE-STATION-40-JET-BLUE-A321-4-1024x768.jpg"),
+      description: "An A320 aircraft under construction, it is green to protect the delicate carbon composite"
+    },
+    link: new URL("https://goo.gl/maps/N79Wf2rJAdQYSziH9"),
+    description: "Went there for a university trip and it was breathtaking. Toulouse has a such lovely mix of french and spanish vibes",
+  },
+  {
+    title: "Mount Snowdon",
+    place: "Scottish Highlands",
+    country: "Scotland",
+    image: {
+      source: new URL("https://i.dailymail.co.uk/i/pix/2012/07/02/article-2167811-0A5798D1000005DC-106_1024x615_large.jpg"),
+      description: "Mount Snowdon with a frozen white peak, clouds roll over the peak as it shines"
+    },
+    link: new URL("https://goo.gl/maps/FXo2G9mfsQ2vpz8NA"),
+    description: "Climbed Mount Snowdon in 2020, it is an achievement I am pround of",
+  }
 ];
 
 export default placesData;
